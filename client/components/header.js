3ab03a36f4de17aa8da30802e464dc02807c42ed
yu-zhen <wlphg010688@gmail.com>
2021-06-22 23:36:18 +0800

    header

--- client/components/header.js
+++ client/components/header.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
+import UserContext from '../context/UserContext';
+import { FaUser } from 'react-icons/fa';
 
 const Header = () => {
+    const { user, setUser } = useContext(UserContext);
+
     return (
         <header>
-            <div className="links">
+            <div className="navLinks">
                 <NavLink to="/" className="link" activeClassName="active" exact>
                     UNIREP SOCIAL
                 </NavLink>
             </div>
+            {user.length == 0? 
+                <div className="navButtons">
+                    <div className="signupButton"> SignUp</div>
+                    <div className="signinButton"> SignIn</div>
+                </div> : 
+                <div className="navButtons">
+                    <div className="userInfo"><FaUser /></div>
+                </div> 
+            }   
         </header>
     );
 }
--- client/router.js
+++ client/router.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+
+import useLocalStorage from './useLocalStorage';
+
 import Header from './components/header';
 import PostsList from './components/postsList';
 
+import UserContext from './context/UserContext';
+
 const Router = () => {
-    // const [books, setBooks] = useLocalStorage('books', []);
+    const [user, setUser] = useLocalStorage('user', {});
     return (
         <BrowserRouter>
             <div>
+            <UserContext.Provider value={{user, setUser}}>
                 <Header />
                 <div className="main-content">
                     <Switch>
                         <Route components={PostsList} path="/" exact={true} />
                     </Switch>
                 </div>
+                </UserContext.Provider>
             </div>
         </BrowserRouter>
     );
