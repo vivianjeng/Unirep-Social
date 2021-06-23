559c82f9cb5f5a38c44db9f5db72288d94b1e33a
yu-zhen <wlphg010688@gmail.com>
2021-06-24 02:36:25 +0800

    sort posts

--- client/components/postsList.js
+++ client/components/postsList.js
@@ -2,10 +2,12 @@ import React from 'react';
 import PostBlock from './postBlock';
 
 const PostsList = (props) => {
+    const sortedPosts = props.posts.sort((a, b) => a.post_time > b.post_time? -1 : 1); // newest show upper
+
     return (
         <div className="post-list">
-            {props.posts.length > 0? (
-                props.posts.map((post) => (
+            {sortedPosts.length > 0? (
+                sortedPosts.map((post) => (
                     <PostBlock key={post.id} post={post} />
                 ))
             ) : <p>No posts are available.</p>}
