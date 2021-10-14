6757e4d51d73219918f0b45d69240bcfd1bc5428
yu-zhen <wlphg010688@gmail.com>
2021-10-14 20:11:38 +0800

    remove import @unirep in default.ts

--- cli/defaults.ts
+++ cli/defaults.ts
@@ -1,18 +1,16 @@
-import { attestingFee, epochLength, numAttestationsPerEpochKey, numEpochKeyNoncePerEpoch } from '@unirep/unirep'
+import { ethers } from 'ethers'; 
 
 const DEFAULT_ETH_PROVIDER = 'http://localhost:8545'
 const DEFAULT_START_BLOCK = 0
-const DEFAULT_MAX_EPOCH_KEY_NONCE = numEpochKeyNoncePerEpoch
-const DEFAULT_NUM_ATTESTATIONS_PER_EPOCH_KEY = numAttestationsPerEpochKey
-const DEFAULT_EPOCH_LENGTH = epochLength
-const DEFAULT_ATTESTING_FEE = attestingFee
+const DEFAULT_MAX_EPOCH_KEY_NONCE = 3
+const DEFAULT_EPOCH_LENGTH = 30
+const DEFAULT_ATTESTING_FEE = ethers.utils.parseEther("0.01")
 const DEFAULT_TREE_DEPTHS_CONFIG = 'circuit'
 
 export {
     DEFAULT_ETH_PROVIDER,
     DEFAULT_START_BLOCK,
     DEFAULT_MAX_EPOCH_KEY_NONCE,
-    DEFAULT_NUM_ATTESTATIONS_PER_EPOCH_KEY,
     DEFAULT_EPOCH_LENGTH,
     DEFAULT_ATTESTING_FEE,
     DEFAULT_TREE_DEPTHS_CONFIG,
